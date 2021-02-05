e5ab897675e604bb952aa3ab2acec93934cb42fe
gdincu <dici0137@scs.ubbcluj.ro>
2021-02-05 21:47:56 +0200

    20210205 Removed the multistep form

--- Client/src/app/store/store.module.ts
+++ Client/src/app/store/store.module.ts
@@ -3,27 +3,11 @@ import { CommonModule } from '@angular/common';
 import { StoreComponent } from './store.component';
 import { BookingDetailsComponent } from './booking-details/booking-details.component';
 import { BookingItemComponent } from './booking-item/booking-item.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { FormlyModule } from '@ngx-formly/core';
-import { FormlyBootstrapModule } from '@ngx-formly/bootstrap';
-import { MatStepperModule } from '@angular/material/stepper';
-import { FormlyFieldStepper } from './stepper.type';
 
 @NgModule({
-  declarations: [StoreComponent, BookingDetailsComponent, BookingItemComponent, FormlyFieldStepper],
+  declarations: [StoreComponent, BookingDetailsComponent, BookingItemComponent],
   imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    MatStepperModule,
-    FormlyBootstrapModule,
-    FormlyModule.forRoot({
-      validationMessages: [
-        { name: 'required', message: 'This field is required' },
-      ],
-      types: [
-        { name: 'stepper', component: FormlyFieldStepper, wrappers: [] },
-      ],
-    }),
+    CommonModule
   ],
   exports: [
     StoreComponent
